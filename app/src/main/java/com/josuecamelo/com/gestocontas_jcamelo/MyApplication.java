a349e25584cb02d5d0db87c996098a91a067ad60
Josué Camelo dos Santos Ferreira <josueprg@gmai.com>
2020-03-21 23:09:22 -0300

    first commit

---
+++ app/src/main/java/com/josuecamelo/com/gestocontas_jcamelo/MyApplication.java
@@ -0,0 +1,19 @@
+package com.josuecamelo.com.gestocontas_jcamelo;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class MyApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration rc = new RealmConfiguration.Builder().
+                name("jcamelo_gestorcontas.realm").build();
+
+        Realm.deleteRealm(rc);
+        Realm.setDefaultConfiguration(rc);
+    }
+}
