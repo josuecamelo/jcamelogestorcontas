73de66505272eec9dc51b02f6e63750c3a299942
Josué Camelo dos Santos Ferreira <josueprg@gmai.com>
2020-03-22 00:54:19 -0300

    Formulário Despesas

---
+++ app/src/main/java/com/josuecamelo/com/gestocontas_jcamelo/ExpenseFormFragment.java
@@ -0,0 +1,28 @@
+package com.josuecamelo.com.gestocontas_jcamelo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ExpenseFormFragment extends Fragment {
+
+    public ExpenseFormFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_expense_form, container, false);
+    }
+}
