a06f9b8390da4a169c48402fb2ebab6dbea77fcd
Josué Camelo dos Santos Ferreira <josueprg@gmai.com>
2020-03-22 00:19:16 -0300

    FABS - Estilização

--- app/src/main/java/com/josuecamelo/com/gestocontas_jcamelo/MainActivity.java
+++ app/src/main/java/com/josuecamelo/com/gestocontas_jcamelo/MainActivity.java
@@ -1,10 +1,22 @@
 package com.josuecamelo.com.gestocontas_jcamelo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fabMain, fabExpenses, fabEarning;
+    private boolean fabsHide = true;
+    private DatePickerDialog.OnDateSetListener dateListner;
+    private Toolbar mToolbar;
+    private DrawerLayout dl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,55 @@ public class MainActivity extends AppCompatActivity {
                 beginTransaction().
                 add(R.id.fragmentContainer, new MainFragment()).
                 commit();
+
+        // Carregar os FABS
+        fabMain = findViewById(R.id.fabMain);
+        fabMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showHideFabs();
+            }
+        });
+
+        fabExpenses = findViewById(R.id.fabExpenses);
+        fabExpenses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fabEarning = findViewById(R.id.fabEarnings);
+        fabEarning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void showHideFabs(){
+        // Define a animação
+        // 0 significa invisivel e 1 visivel
+        // Indo de 0 a 1
+        AlphaAnimation fadeAnimation = new AlphaAnimation(0,1);
+        // Define a duração
+        fadeAnimation.setDuration(1000);
+        // Define o atraso
+        fadeAnimation.setStartOffset(100);
+
+        if(fabsHide){
+            fabExpenses.setVisibility(View.VISIBLE);
+            fabEarning.setVisibility(View.VISIBLE);
+            fabExpenses.setAnimation(fadeAnimation);
+            fabEarning.setAnimation(fadeAnimation);
+        }else{
+            fabExpenses.clearAnimation();
+            fabEarning.clearAnimation();
+            fabExpenses.setVisibility(View.GONE);
+            fabEarning.setVisibility(View.GONE);
+        }
+
+        fabsHide = !fabsHide;
     }
 }
